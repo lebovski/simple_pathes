b9d4176891ba7c1f4c427345dbab420cadfd7d20
alx <alx.nvc@gmail.com>
2018-02-11 13:37:17 +0000

    SP-0 | get count of paths

--- graph/graph.go
+++ graph/graph.go
@@ -1,1 +1,20 @@
 package graph
+
+// Маторица смежности
+type graph [][]bool
+//type pathes [][]int
+
+func dfs(graph graph, visited []bool, n int, v int, x int, cnt int) int {
+	if v == x {
+		cnt++
+		return cnt
+	}
+	visited[v] = true
+	for i := 0; i <= n; i++ {
+		if graph[v][i] && visited[i] {
+			cnt = dfs(graph, visited, n, i, x, cnt)
+		}
+	}
+	visited[v] = false
+	return cnt
+}
