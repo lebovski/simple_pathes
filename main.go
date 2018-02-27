379b58c0320358a3a1628ade23a1b5d817716a71
alexandr.novichkov <alexandr.novichkov@ncloudtech.ru>
2018-02-27 15:45:53 +0000

    SP-2-0 | Add generate cycling

--- main.go
+++ main.go
@@ -26,19 +26,20 @@ func main() {
 		fmt.Printf("\n%v", v)
 	}
 
-	edges := utils.GetEdges("./utils/example.yml")
+	edges := utils.GetEdges("./utils/example.yml", 0)
 	brokenRes := edges.DFS(
-		utils.Vertex{"INITIAL", "state", false, 0},
-		utils.Vertex{"BROKEN", "state", false, 0},
+		utils.Vertex{"INITIAL", "state", false, 0, 0},
+		utils.Vertex{"BROKEN", "state", false, 0, 0},
 	)
 	fmt.Printf("\n\nBroken count: %v", len(brokenRes))
 	fmt.Printf("\n%v", "====== Paths:")
 	getPaths(brokenRes)
 
 	finishedRes := edges.DFS(
-		utils.Vertex{"INITIAL", "state", false, 0},
-		utils.Vertex{"FINISHED", "state", false, 0},
+		utils.Vertex{"INITIAL", "state", false, 0, 0},
+		utils.Vertex{"FINISHED", "state", false, 0, 0},
 	)
+
 	fmt.Printf("\n\nFinished count: %v", len(finishedRes))
 	fmt.Printf("\n%v", "====== Paths:")
 	getPaths(finishedRes)
@@ -49,9 +50,11 @@ func getPaths(finishedRes [][]interface{}) {
 		res := make([]string, 0, len(v))
 		for _, ver := range v {
 			vv := ver.(utils.Vertex)
-			if vv.Loop {
-				res = append(res, "Chunk")
-				continue
+			if vv.Loop && vv.Type == "state" {
+				for ii := 0; ii <= vv.LoopCount; ii++ {
+					res = append(res, "Chunk")
+					continue
+				}
 			}
 			if vv.Type == "action" {
 				res = append(res, vv.Name)
--- utils/yaml_parser.go
+++ utils/yaml_parser.go
@@ -11,13 +11,14 @@ import (
 type States map[string][]map[string][]map[string]string
 
 type Vertex struct {
-	Name  string
-	Type  string
-	Loop  bool
-	Index int
+	Name      string
+	Type      string
+	Loop      bool
+	LoopCount int
+	Index     int
 }
 
-func GetEdges(ymlPath string) graph.Edges {
+func GetEdges(ymlPath string, loopCount int) graph.Edges {
 	var edges graph.Edges
 
 	states := unmarshalYaml(ymlPath)
@@ -27,15 +28,19 @@ func GetEdges(ymlPath string) graph.Edges {
 			for _, action := range actions {
 				var st0, st1, act Vertex
 
-				act = Vertex{Name: action["action"], Type: "action", Loop: false, Index: stateIndex}
-				st0 = Vertex{Name: state, Type: "state", Loop: loopElements[state], Index: 0}
-				st1 = Vertex{Name: action["result"], Type: "state", Loop: loopElements[action["result"]], Index: 0}
+				act = Vertex{Name: action["action"], Type: "action", Loop: false, LoopCount: 0, Index: stateIndex}
+				st0 = Vertex{Name: state, Type: "state", Loop: loopElements[state], LoopCount: 0, Index: 0}
+				st1 = Vertex{Name: action["result"], Type: "state", Loop: loopElements[action["result"]], LoopCount: 0, Index: 0}
 
-				edges = append(edges, graph.Edge{st0, act})
-				edges = append(edges, graph.Edge{act, st1})
+				if state != action["result"] {
+					edges = append(edges, graph.Edge{st0, act})
+					edges = append(edges, graph.Edge{act, st1})
+				}
 			}
 		}
 	}
+
+	appendCicledEdges(loopElements, &edges, loopCount)
 
 	return edges
 }
@@ -69,4 +74,46 @@ func unmarshalYaml(ymlPath string) States {
 	}
 
 	return states
+}
+
+func appendCicledEdges(loopVertexes map[string]bool, edges *graph.Edges, loopCount int) {
+	for vertexName := range loopVertexes {
+		vertex := Vertex{Name: vertexName, Type: "state", Loop: true, LoopCount: 0, Index: 0}
+		inEdges := getInEdges(*edges, vertex)
+		outEdges := getOutEdges(*edges, vertex)
+
+		for _, in := range inEdges {
+			for _, out := range outEdges {
+				for count := 0; count <= loopCount; count++ {
+					if count == 0 {
+						*edges = append(*edges, graph.Edge{in[0], out[1]})
+					} else {
+						newVertex := Vertex{Name: vertexName, Type: "state", Loop: true, LoopCount: count, Index: 0}
+						*edges = append(*edges, graph.Edge{in[0], newVertex})
+						*edges = append(*edges, graph.Edge{newVertex, out[1]})
+					}
+				}
+			}
+		}
+	}
+}
+
+func getInEdges(edges graph.Edges, vertex Vertex) []graph.Edge {
+	var res []graph.Edge
+	for _, edge := range edges {
+		if edge[1] == vertex {
+			res = append(res, edge)
+		}
+	}
+	return res
+}
+
+func getOutEdges(edges graph.Edges, vertex Vertex) []graph.Edge {
+	var res []graph.Edge
+	for _, edge := range edges {
+		if edge[0] == vertex {
+			res = append(res, edge)
+		}
+	}
+	return res
 }
